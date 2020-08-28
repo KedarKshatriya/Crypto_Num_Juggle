b69d4b30239b02647818ee8db52e4fda39f44233
Rutuja Ajagekar <rutuajagekar99@gmail.com>
2020-08-28 18:07:52 +0530

    formatting

--- js/tile.js
+++ js/tile.js
@@ -25,3 +25,13 @@ Tile.prototype.serialize = function () {
     value: this.value
   };
 };
+
+
+function help() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
